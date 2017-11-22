76151822bd602f895e093d421dec8ebf6261f6c3
Chondrogiannis Theodoros <thondrogiannis@unibz.it>
2017-11-22 14:04:23 +0100

    license

--- ch/SubgraphCH.hpp
+++ ch/SubgraphCH.hpp
@@ -1,21 +1,21 @@
-/* Copyright (C) 2005, 2006, 2007, 2008 
- * Robert Geisberger, Dominik Schultes, Peter Sanders,
- * Universitaet Karlsruhe (TH)
+/* Copyright (C) 2017
+ * Theodoros Chondrogiannis
+ * Free University of Bozen-Bolzano
  *
- * This file is part of Contraction Hierarchies.
+ * This file is part of ParDiSP.
  *
- * Contraction Hierarchies is free software; you can redistribute it
+ * ParDiSP is free software; you can redistribute it
  * and/or modify it under the terms of the GNU Affero General Public License
  * as published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
- * Contraction Hierarchies is distributed in the hope that it will be
+ * ParDiSP is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Contraction Hierarchies; see the file COPYING; if not,
+ * along with ParDiSP; see the file COPYING; if not,
  * see <http://www.gnu.org/licenses/>.
  */
 
